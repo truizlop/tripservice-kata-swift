9c1bed280f3e6b581048dd693545a41e7bf09064
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-04-24 20:33:43 +0200

    Extract methods to increase readability of code

--- TripService-Kata/TripService.swift
+++ TripService-Kata/TripService.swift
@@ -19,12 +19,16 @@ class TripService {
         guard let loggedUser = loggedUser
         else{ throw TripServiceError.UserNotLoggedIn }
         
-        var tripList : [Trip] = []
-        
-        if user.isFriendWith(loggedUser) {
-            tripList = try! tripDAO.findTripsBy(user)
-        }
-        return tripList
-        
+        return user.isFriendWith(loggedUser) ?
+                try! tripsBy(user) :
+                noTrips()
+    }
+    
+    private func tripsBy(user : User) throws -> [Trip] {
+        return try! tripDAO.findTripsBy(user)
+    }
+    
+    private func noTrips() -> [Trip] {
+        return []
     }
 }
