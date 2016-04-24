0e48f485d48570d33efcbc539a7166847012b111
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-04-24 18:58:55 +0200

    Initial Commit

---
+++ TripService-Kata/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TripService-Kata
+//
+//  Created by Tomás Ruiz López on 24/4/16.
+//  Copyright © 2016 Tomás Ruiz López. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
